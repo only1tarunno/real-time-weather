4702ce24a87c58aeb0111cdf2816d2816a9803cf
only1tarunno <tarunno281@gmail.com>
2024-02-10 14:21:47 +0600

    board create

--- src/Page/Page.jsx
+++ src/Page/Page.jsx
@@ -1,9 +1,15 @@
 import Header from "../components/Header/Header";
+import WeatherBoard from "../components/weather/WeatherBoard";
 
 const Page = () => {
   return (
     <>
       <Header />
+      <main>
+        <section className="">
+          <WeatherBoard />
+        </section>
+      </main>
     </>
   );
 };
