09bd8ea5106d12c845779a8fac2e58c6c980202b
only1tarunno <tarunno281@gmail.com>
2024-02-10 15:05:14 +0600

    hook create

--- src/Page/Page.jsx
+++ src/Page/Page.jsx
@@ -3,14 +3,14 @@ import WeatherBoard from "../components/weather/WeatherBoard";
 
 const Page = () => {
   return (
-    <>
+    <div className="grid place-items-center h-screen">
       <Header />
       <main>
         <section className="">
           <WeatherBoard />
         </section>
       </main>
-    </>
+    </div>
   );
 };
 
