40b1a781d75a502409eeb67634b1bc6273876e03
only1tarunno <tarunno281@gmail.com>
2024-02-11 21:35:37 +0600

    done

--- src/components/Header/SearchBar.jsx
+++ src/components/Header/SearchBar.jsx
@@ -1,31 +1,33 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import search from "../../assets/search.svg";
 import { LocationContext } from "../../contex";
 import { getLocationByName } from "../../data/location-data";
+import useDebounce from "../../hooks/useDebouncing";
 
 const SearchBar = () => {
-  const [searchterm, setSearchTerm] = useState();
   const { setSelectedLocation } = useContext(LocationContext);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    const fetchedLocation = getLocationByName(searchterm);
-    console.log(fetchedLocation);
+  const doSearch = useDebounce((term) => {
+    const fetchedLocation = getLocationByName(term);
     setSelectedLocation({ ...fetchedLocation });
+  }, 500);
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    doSearch(value);
   };
 
   return (
-    <form action="#" onSubmit={handleSubmit}>
+    <form action="#">
       <div className="flex items-center space-x-2 py-2 px-3 group focus-within:bg-black/30 transition-all border-b border-white/50 focus-within:border-b-0 focus-within:rounded-md">
         <input
           className="bg-transparent placeholder:text-white text-white w-full text-xs md:text-base outline-none border-none"
           type="search"
-          onChange={(e) => setSearchTerm(e.target.value)}
+          onChange={handleChange}
           placeholder="Search Location"
           required
         />
-        <button type="submit">
+        <button>
           <img src={search} />
         </button>
       </div>
