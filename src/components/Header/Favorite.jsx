5a86674b3feda8cb01711311fd3496ce36f32511
only1tarunno <tarunno281@gmail.com>
2024-02-11 11:06:32 +0600

    show modal

--- src/components/Header/Favorite.jsx
+++ src/components/Header/Favorite.jsx
@@ -1,11 +1,15 @@
+/* eslint-disable react/prop-types */
 import fav from "../../assets/heart.svg";
 
-const Favorite = () => {
+const Favorite = ({ onShow }) => {
   return (
-    <div className="p-2 hover:bg-black/30 cursor-pointer flex gap-2 items-center rounded-md transition-all">
+    <button
+      onClick={onShow}
+      className="p-2 hover:bg-black/30 cursor-pointer flex gap-2 items-center rounded-md transition-all"
+    >
       <img src={fav} alt="" />
       <span>Favourite Locations</span>
-    </div>
+    </button>
   );
 };
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
+import { useState } from "react";
 import Favorite from "./Favorite";
 import FavouriteListModal from "./FavouriteListModal";
 import Logo from "./Logo";
 import SearchBar from "./SearchBar";
 
 const Header = () => {
+  const [showFavModal, setShowFavModal] = useState(false);
   return (
     <header className="fixed w-full top-0 z-50 bg-gradient-to-b from-black/60 to-black/0 pb-10">
       <nav className="container flex items-center justify-between py-6">
@@ -11,10 +13,10 @@ const Header = () => {
 
         <div className="flex items-center gap-4 relative">
           <SearchBar />
-          <Favorite />
+          <Favorite onShow={() => setShowFavModal(!showFavModal)} />
 
           {/* <!-- Modal --> */}
-          <FavouriteListModal />
+          {showFavModal && <FavouriteListModal />}
         </div>
       </nav>
     </header>
